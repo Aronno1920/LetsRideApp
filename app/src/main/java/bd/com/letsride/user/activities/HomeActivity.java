e1e2a0d4d2a167922de1d9b78be402b3ee24cbd8
Selim Ahmed <aronno1920@hotmail.com>
2022-01-16 23:55:28 +0600

    Press double back -> exit update

--- app/src/main/java/bd/com/letsride/user/activities/HomeActivity.java
+++ app/src/main/java/bd/com/letsride/user/activities/HomeActivity.java
@@ -3,7 +3,10 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import bd.com.letsride.user.R;
 import bd.com.letsride.user.fragments.CreditFragment;
@@ -16,7 +19,7 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 public class HomeActivity extends BaseActivity {
 
     BottomNavigationView bottomNavigationView;
-    //final String TAG = HomeActivity.this.getPackageName();
+    boolean doubleBackToExitPressedOnce = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +28,7 @@ public class HomeActivity extends BaseActivity {
         initializeUI();
         registerActions();
 
-        loadFragment(DashboardFragment.newInstance("",""));
+        loadFragment(DashboardFragment.newInstance("", ""));
     }
 
     private void registerActions() {
@@ -34,13 +37,13 @@ public class HomeActivity extends BaseActivity {
             public boolean onNavigationItemSelected(MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.home:
-                        loadFragment(DashboardFragment.newInstance("",""));
+                        loadFragment(DashboardFragment.newInstance("", ""));
                         break;
                     case R.id.credit:
-                        loadFragment(CreditFragment.newInstance("",""));
+                        loadFragment(CreditFragment.newInstance("", ""));
                         break;
                     case R.id.routes:
-                        loadFragment(RoutesFragment.newInstance("",""));
+                        loadFragment(RoutesFragment.newInstance("", ""));
                         break;
                 }
                 return true;
@@ -57,5 +60,24 @@ public class HomeActivity extends BaseActivity {
         bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setBackground(null);
         bottomNavigationView.getMenu().getItem(2).setEnabled(false);
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (doubleBackToExitPressedOnce) {
+            super.onBackPressed();
+            return;
+        }
+
+        this.doubleBackToExitPressedOnce = true;
+        Toast.makeText(this, "Please click BACK again to exit", Toast.LENGTH_SHORT).show();
+
+        new Handler(Looper.getMainLooper()).postDelayed(new Runnable() {
+
+            @Override
+            public void run() {
+                doubleBackToExitPressedOnce = false;
+            }
+        }, 2000);
     }
 }
