8d8647cab946442c0e8a81a2e42437fbe20c1b36
Selim Ahmed <aronno1920@hotmail.com>
2022-01-20 17:28:40 +0600

    Official Update (20 January, 2022)

---
+++ app/src/main/java/bd/com/letsride/user/adapters/RideDetailsAdapter.java
@@ -0,0 +1,94 @@
+package bd.com.letsride.user.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+import bd.com.letsride.user.R;
+import bd.com.letsride.user.models.RideDetailsModel;
+
+public class RideDetailsAdapter extends RecyclerView.Adapter<RideDetailsAdapter.MyViewHolder> {
+
+    private Context mContext;
+    private List<RideDetailsAdapter> albumList;
+
+    public class MyViewHolder extends RecyclerView.ViewHolder {
+        public TextView title, count;
+
+        public MyViewHolder(View view) {
+            super(view);
+            title = (TextView) view.findViewById(R.id.title);
+            count = (TextView) view.findViewById(R.id.count);
+            thumbnail = (ImageView) view.findViewById(R.id.thumbnail);
+            overflow = (ImageView) view.findViewById(R.id.overflow);
+        }
+    }
+
+
+    public RideDetailsAdapter(Context mContext, List<RideDetailsAdapter> albumList) {
+        this.mContext = mContext;
+        this.albumList = albumList;
+    }
+
+    @Override
+    public MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.album_card, parent, false);
+
+        return new MyViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(final MyViewHolder holder, int position) {
+        Album album = albumList.get(position);
+        holder.title.setText(album.getName());
+        holder.count.setText(album.getNumOfSongs() + " songs");
+
+        // loading album cover using Glide library
+        Glide.with(mContext).load(album.getThumbnail()).into(holder.thumbnail);
+
+        holder.overflow.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPopupMenu(holder.overflow);
+            }
+        });
+    }
+
+    /**
+     * Showing popup menu when tapping on 3 dots
+     */
+//
+
+    /**
+     * Click listener for popup menu items
+     */
+//    class MyMenuItemClickListener implements PopupMenu.OnMenuItemClickListener {
+//
+//        public MyMenuItemClickListener() {
+//        }
+//
+//        @Override
+//        public boolean onMenuItemClick(MenuItem menuItem) {
+//            switch (menuItem.getItemId()) {
+//                case R.id.action_add_favourite:
+//                    Toast.makeText(mContext, "Add to favourite", Toast.LENGTH_SHORT).show();
+//                    return true;
+//                case R.id.action_play_next:
+//                    Toast.makeText(mContext, "Play next", Toast.LENGTH_SHORT).show();
+//                    return true;
+//                default:
+//            }
+//            return false;
+//        }
+//    }
+
+    @Override
+    public int getItemCount() {
+        return albumList.size();
+    }
+}
---
+++ app/src/main/java/bd/com/letsride/user/models/RideDetailsModel.java
@@ -0,0 +1,58 @@
+package bd.com.letsride.user.models;
+
+public class RideDetailsModel {
+
+    private String ZoneName;
+    private String RideDate;
+    private String RideTime;
+    private String RidePrice;
+    private String DriverName;
+
+    public RideDetailsModel(String zoneName, String rideDate, String rideTime, String ridePrice, String driverName) {
+        ZoneName = zoneName;
+        RideDate = rideDate;
+        RideTime = rideTime;
+        RidePrice = ridePrice;
+        DriverName = driverName;
+    }
+
+    public String getZoneName() {
+        return ZoneName;
+    }
+
+    public void setZoneName(String zoneName) {
+        ZoneName = zoneName;
+    }
+
+    public String getRideDate() {
+        return RideDate;
+    }
+
+    public void setRideDate(String rideDate) {
+        RideDate = rideDate;
+    }
+
+    public String getRideTime() {
+        return RideTime;
+    }
+
+    public void setRideTime(String rideTime) {
+        RideTime = rideTime;
+    }
+
+    public String getRidePrice() {
+        return RidePrice;
+    }
+
+    public void setRidePrice(String ridePrice) {
+        RidePrice = ridePrice;
+    }
+
+    public String getDriverName() {
+        return DriverName;
+    }
+
+    public void setDriverName(String driverName) {
+        DriverName = driverName;
+    }
+}
